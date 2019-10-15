a383c24727524b9c2b1b9b1378d5259bd1ccd110
Dan <doates625@gmail.com>
2019-10-15 01:07:01 -0400

    Massive refactor tested on Arduino and Mbed devices

--- Bno055.cpp
+++ Bno055.cpp
@@ -7,9 +7,11 @@
 /**
  * Static Constants
  */
-const float BNO055::acc_per_cnt = 0.01000000000;
-const float BNO055::gyr_per_cnt = 0.00111111111;
-const float BNO055::eul_per_cnt = 0.00109170305;
+const float BNO055::acc_per_lsb = 1.0f / 100.0f;
+const float BNO055::mag_per_lsb = 1.0f / 16.0f;
+const float BNO055::gyr_per_lsb = 1.0f / 900.0f;
+const float BNO055::eul_per_lsb = 1.0f / 900.0f;
+const float BNO055::qua_per_lsb = 1.0f / 16384.0f;
 
 /**
  * @brief Constructs BNO055 interface
@@ -66,75 +68,140 @@ bool BNO055::init()
 }
 
 /**
- * @brief Reads accelerometer, gyro, and euler angles from device
+ * @brief Updates all IMU readings
  */
 void BNO055::update()
 {
-	// Read Accelerometer
+#if defined(PLATFORM_ARDUINO)
+	i2c.read_sequence(reg_acc_x_addr, 32);
+	read_acc();
+	read_mag();
+	read_gyr();
+	read_eul();
+	read_qua();
+	i2c.read_sequence(reg_lia_x_addr, 12);
+	read_lia();
+	read_grv();
+#elif defined(PLATFORM_MBED)
+	i2c.read_sequence(reg_acc_x_addr, 44);
+	read_acc();
+	read_mag();
+	read_gyr();
+	read_eul();
+	read_qua();
+	read_lia();
+	read_grv();
+#endif
+}
+
+/**
+ * @brief Updates accelerometer readings
+ */
+void BNO055::update_acc()
+{
 	i2c.read_sequence(reg_acc_x_addr, 6);
-	acc_x = i2c.read_int16() * acc_per_cnt;
-	acc_y = i2c.read_int16() * acc_per_cnt;
-	acc_z = i2c.read_int16() * acc_per_cnt;
-
-	// Read Gyroscope
-	i2c.read_sequence(reg_gyro_x_addr, 6);
-	vel_x = i2c.read_int16() * gyr_per_cnt;
-	vel_y = i2c.read_int16() * gyr_per_cnt;
-	vel_z = i2c.read_int16() * gyr_per_cnt;
-
-	// Read Euler Angles
-	i2c.read_sequence(reg_euler_h_addr, 6);
-	eul_h = i2c.read_int16() * eul_per_cnt;
-	eul_r = i2c.read_int16() * eul_per_cnt;
-	eul_p = i2c.read_int16() * eul_per_cnt;
+	read_acc();
 }
 
 /**
- * @brief Returns x-velocity [rad/s]
- */
-float BNO055::get_vel_x()
-{
-	return vel_x;
-}
-
-/**
- * @brief Returns y-velocity [rad/s]
- */
-float BNO055::get_vel_y()
-{
-	return vel_y;
-}
-
-/**
- * @brief Returns z-velocity [rad/s]
- */
-float BNO055::get_vel_z()
-{
-	return vel_z;
-}
-
-/**
- * @brief returns x-acceleration [m/s^2]
+ * @brief Returns X acceleration [m/s^2]
  */
 float BNO055::get_acc_x()
 {
-	return acc_x;
+	return read_acc_x ? acc_x : i2c.read_int16(reg_acc_x_addr) * acc_per_lsb;
 }
 
 /**
- * @brief returns y-acceleration [m/s^2]
+ * @brief Returns Y acceleration [m/s^2]
  */
 float BNO055::get_acc_y()
 {
-	return acc_y;
+	return read_acc_y ? acc_y : i2c.read_int16(reg_acc_y_addr) * acc_per_lsb;
 }
 
 /**
- * @brief returns z-acceleration [m/s^2]
+ * @brief Returns Z acceleration [m/s^2]
  */
 float BNO055::get_acc_z()
 {
-	return acc_z;
+	return read_acc_z ? acc_z : i2c.read_int16(reg_acc_z_addr) * acc_per_lsb;
+}
+
+// Magnetometer
+
+/**
+ * @brief Updates magnetometer readings
+ */
+void BNO055::update_mag()
+{
+	i2c.read_sequence(reg_mag_x_addr, 6);
+	read_mag();
+}
+
+/**
+ * @brief Returns X magnetic field [uT]
+ */
+float BNO055::get_mag_x()
+{
+	return read_mag_x ? mag_x : i2c.read_int16(reg_mag_x_addr) * mag_per_lsb;
+}
+
+/**
+ * @brief Returns Y magnetic field [uT]
+ */
+float BNO055::get_mag_y()
+{
+	return read_mag_y ? mag_y : i2c.read_int16(reg_mag_y_addr) * mag_per_lsb;
+}
+
+/**
+ * @brief Returns Z magnetic field [uT]
+ */
+float BNO055::get_mag_z()
+{
+	return read_mag_z ? mag_z : i2c.read_int16(reg_mag_z_addr) * mag_per_lsb;
+}
+
+/**
+ * @brief Updates gyroscope readings
+ */
+void BNO055::update_gyr()
+{
+	i2c.read_sequence(reg_gyr_x_addr, 6);
+	read_gyr();
+}
+
+/**
+ * @brief Returns X angular velocity [rad/s]
+ */
+float BNO055::get_gyr_x()
+{
+	return read_gyr_x ? gyr_x : i2c.read_int16(reg_gyr_x_addr) * gyr_per_lsb;
+}
+
+/**
+ * @brief Returns Y angular velocity [rad/s]
+ */
+float BNO055::get_gyr_y()
+{
+	return read_gyr_y ? gyr_y : i2c.read_int16(reg_gyr_y_addr) * gyr_per_lsb;
+}
+
+/**
+ * @brief Returns Z angular velocity [rad/s]
+ */
+float BNO055::get_gyr_z()
+{
+	return read_gyr_z ? gyr_z : i2c.read_int16(reg_gyr_z_addr) * gyr_per_lsb;
+}
+
+/**
+ * @brief Updates euler angle readings
+ */
+void BNO055::update_eul()
+{
+	i2c.read_sequence(reg_eul_h_addr, 6);
+	read_eul();
 }
 
 /**
@@ -142,15 +209,7 @@ float BNO055::get_acc_z()
  */
 float BNO055::get_eul_h()
 {
-	return eul_h;
-}
-
-/**
- * @brief Returns euler pitch [rad]
- */
-float BNO055::get_eul_p()
-{
-	return eul_p;
+	return read_eul_h ? eul_h : i2c.read_int16(reg_eul_h_addr) * eul_per_lsb;
 }
 
 /**
@@ -158,5 +217,191 @@ float BNO055::get_eul_p()
  */
 float BNO055::get_eul_r()
 {
-	return eul_r;
+	return read_eul_r ? eul_r : i2c.read_int16(reg_eul_r_addr) * eul_per_lsb;
+}
+
+/**
+ * @brief Returns euler pitch [rad]
+ */
+float BNO055::get_eul_p()
+{
+	return read_eul_p ? eul_p : i2c.read_int16(reg_eul_p_addr) * eul_per_lsb;
+}
+
+/**
+ * @brief Updates quaternion readings
+ */
+void BNO055::update_qua()
+{
+	i2c.read_sequence(reg_qua_x_addr, 8);
+	read_qua();
+}
+
+/**
+ * @brief Returns quaternion W
+ */
+float BNO055::get_qua_w()
+{
+	return read_qua_w ? qua_w : i2c.read_int16(reg_qua_w_addr) * qua_per_lsb;
+}
+
+/**
+ * @brief Returns quaternion X
+ */
+float BNO055::get_qua_x()
+{
+	return read_qua_x ? qua_x : i2c.read_int16(reg_qua_x_addr) * qua_per_lsb;
+}
+
+/**
+ * @brief Returns quaternion Y
+ */
+float BNO055::get_qua_y()
+{
+	return read_qua_y ? qua_y : i2c.read_int16(reg_qua_y_addr) * qua_per_lsb;
+}
+
+/**
+ * @brief Returns quaternion Z
+ */
+float BNO055::get_qua_z()
+{
+	return read_qua_z ? qua_z : i2c.read_int16(reg_qua_z_addr) * qua_per_lsb;
+}
+
+/**
+ * @brief Updates linear acceleration readings
+ */
+void BNO055::update_lia()
+{
+	i2c.read_sequence(reg_lia_x_addr, 6);
+	read_lia();
+}
+
+/**
+ * @brief Returns X linear acceleration [m/s^2]
+ */
+float BNO055::get_lia_x()
+{
+	return read_lia_x ? lia_x : i2c.read_int16(reg_lia_x_addr) * acc_per_lsb;
+}
+
+/**
+ * @brief Returns Y linear acceleration [m/s^2]
+ */
+float BNO055::get_lia_y()
+{
+	return read_lia_y ? lia_y : i2c.read_int16(reg_lia_y_addr) * acc_per_lsb;
+}
+
+/**
+ * @brief Returns Z linear acceleration [m/s^2]
+ */
+float BNO055::get_lia_z()
+{
+	return read_lia_z ? lia_z : i2c.read_int16(reg_lia_z_addr) * acc_per_lsb;
+}
+
+/**
+ * @brief Updates gravity vector readings
+ */
+void BNO055::update_grv()
+{
+	i2c.read_sequence(reg_grv_x_addr, 6);
+	read_grv();
+}
+
+/**
+ * @brief Returns gravity vector X [m/s^2]
+ */
+float BNO055::get_grv_x()
+{
+	return read_grv_x ? grv_x : i2c.read_int16(reg_grv_x_addr) * acc_per_lsb;
+}
+
+/**
+ * @brief Returns gravity vector Y [m/s^2]
+ */
+float BNO055::get_grv_y()
+{
+	return read_grv_y ? grv_y : i2c.read_int16(reg_grv_y_addr) * acc_per_lsb;
+}
+
+/**
+ * @brief Returns gravity vector Z [m/s^2]
+ */
+float BNO055::get_grv_z()
+{
+	return read_grv_z ? grv_z : i2c.read_int16(reg_grv_z_addr) * acc_per_lsb;
+}
+
+/**
+ * @brief Reads acc registers after call to I2CDevice::read_sequence()
+ */
+void BNO055::read_acc()
+{
+	acc_x = i2c.read_int16() * acc_per_lsb; read_acc_x = true;
+	acc_y = i2c.read_int16() * acc_per_lsb; read_acc_y = true;
+	acc_z = i2c.read_int16() * acc_per_lsb; read_acc_z = true;
+}
+
+/**
+ * @brief Reads mag registers after call to I2CDevice::read_sequence()
+ */
+void BNO055::read_mag()
+{
+	mag_x = i2c.read_int16() * mag_per_lsb; read_mag_x = true;
+	mag_y = i2c.read_int16() * mag_per_lsb; read_mag_y = true;
+	mag_z = i2c.read_int16() * mag_per_lsb; read_mag_z = true;
+}
+
+/**
+ * @brief Reads gyr registers after call to I2CDevice::read_sequence()
+ */
+void BNO055::read_gyr()
+{
+	gyr_x = i2c.read_int16() * gyr_per_lsb; read_gyr_x = true;
+	gyr_y = i2c.read_int16() * gyr_per_lsb; read_gyr_y = true;
+	gyr_z = i2c.read_int16() * gyr_per_lsb; read_gyr_z = true;
+}
+
+/**
+ * @brief Reads eul registers after call to I2CDevice::read_sequence()
+ */
+void BNO055::read_eul()
+{
+	eul_h = i2c.read_int16() * eul_per_lsb; read_eul_h = true;
+	eul_r = i2c.read_int16() * eul_per_lsb; read_eul_r = true;
+	eul_p = i2c.read_int16() * eul_per_lsb; read_eul_p = true;
+}
+
+/**
+ * @brief Reads qua registers after call to I2CDevice::read_sequence()
+ */
+void BNO055::read_qua()
+{
+	qua_w = i2c.read_int16() * qua_per_lsb; read_qua_w = true;
+	qua_x = i2c.read_int16() * qua_per_lsb; read_qua_x = true;
+	qua_y = i2c.read_int16() * qua_per_lsb; read_qua_y = true;
+	qua_z = i2c.read_int16() * qua_per_lsb; read_qua_z = true;
+}
+
+/**
+ * @brief Reads lia registers after call to I2CDevice::read_sequence()
+ */
+void BNO055::read_lia()
+{
+	lia_x = i2c.read_int16() * acc_per_lsb; read_lia_x = true;
+	lia_y = i2c.read_int16() * acc_per_lsb; read_lia_y = true;
+	lia_z = i2c.read_int16() * acc_per_lsb; read_lia_z = true;
+}
+
+/**
+ * @brief Reads grv registers after call to I2CDevice::read_sequence()
+ */
+void BNO055::read_grv()
+{
+	grv_x = i2c.read_int16() * acc_per_lsb; read_grv_x = true;
+	grv_y = i2c.read_int16() * acc_per_lsb; read_grv_y = true;
+	grv_z = i2c.read_int16() * acc_per_lsb; read_grv_z = true;
 }
--- Bno055.h
+++ Bno055.h
@@ -30,6 +30,18 @@
 #pragma once
 #include <I2CDevice.h>
 
+/**
+ * I2CDevice Macro Update
+ */
+#if defined(PLATFORM_MBED) && I2CDEVICE_BUFFER_SIZE < 44
+	#warning BNO055 requires I2CDEVICE_BUFFER_SIZE >= 44. Setting to 44...
+	#undef I2CDEVICE_BUFFER_SIZE
+	#define I2CDEVICE_BUFFER_SIZE 44
+#endif
+
+/**
+ * Class Declaration
+ */
 class BNO055
 {
 public:
@@ -48,31 +60,68 @@ public:
 	}
 	axis_config_t;
 
-	// Constructor and Initialization
+	// Constructor and Basics
 	BNO055(I2CDEVICE_I2C_CLASS* i2c, axis_config_t axis_config = tlf);
 	bool init();
-
-	// Data Reading
 	void update();
-	float get_vel_x();
-	float get_vel_y();
-	float get_vel_z();
+
+	// Accelerometer
+	void update_acc();
 	float get_acc_x();
 	float get_acc_y();
 	float get_acc_z();
+
+	// Magnetometer
+	void update_mag();
+	float get_mag_x();
+	float get_mag_y();
+	float get_mag_z();
+
+	// Gyroscope
+	void update_gyr();
+	float get_gyr_x();
+	float get_gyr_y();
+	float get_gyr_z();
+
+	// Euler Angles
+	void update_eul();
 	float get_eul_h();
-	float get_eul_p();
 	float get_eul_r();
+	float get_eul_p();
+
+	// Quaternion
+	void update_qua();
+	float get_qua_w();
+	float get_qua_x();
+	float get_qua_y();
+	float get_qua_z();
+
+	// Linear Acceleration
+	void update_lia();
+	float get_lia_x();
+	float get_lia_y();
+	float get_lia_z();
+
+	// Gravity Vector
+	void update_grv();
+	float get_grv_x();
+	float get_grv_y();
+	float get_grv_z();
 
 protected:
 
-	// I2C Registers
+	// I2C Communication
 	static const uint8_t i2c_addr = 0x28;
+	I2CDevice i2c;
+
+	// I2C ID Registers
 	static const uint8_t reg_id_addr = 0x00;
 	static const uint8_t reg_id_chip = 0xA0;
 	static const uint8_t reg_id_acc = 0xFB;
 	static const uint8_t reg_id_mag = 0x32;
 	static const uint8_t reg_id_gyr = 0x0F;
+
+	// I2C Config Registers
 	static const uint8_t reg_pwrmode_addr = 0x3E;
 	static const uint8_t reg_pwrmode_norm = 0x00;
 	static const uint8_t reg_opmode_addr = 0x3D;
@@ -80,25 +129,75 @@ protected:
 	static const uint8_t reg_opmode_ndof = 0x0C;
 	static const uint8_t reg_axis_config_addr = 0x41;
 	static const uint8_t reg_axis_sign_addr = 0x42;
-	static const uint8_t reg_acc_x_addr = 0x28;
-	static const uint8_t reg_acc_y_addr = 0x2A;
-	static const uint8_t reg_acc_z_addr = 0x2C;
-	static const uint8_t reg_euler_h_addr = 0x1A;
-	static const uint8_t reg_euler_r_addr = 0x1C;
-	static const uint8_t reg_euler_p_addr = 0x1E;
-	static const uint8_t reg_gyro_x_addr = 0x14;
-	static const uint8_t reg_gyro_y_addr = 0x16;
-	static const uint8_t reg_gyro_z_addr = 0x18;
-
-	// Unit Conversions
-	static const float acc_per_cnt;
-	static const float gyr_per_cnt;
-	static const float eul_per_cnt;
-
-	// Class Members
-	I2CDevice i2c;
 	axis_config_t axis_config;
-	float vel_x, vel_y, vel_z;
-	float acc_x, acc_y, acc_z;
-	float eul_h, eul_p, eul_r;
+
+	// Accelerometer
+	static const float acc_per_lsb;
+	static const uint8_t reg_acc_x_addr = 0x08;
+	static const uint8_t reg_acc_y_addr = 0x0A;
+	static const uint8_t reg_acc_z_addr = 0x0C;
+	float acc_x; bool read_acc_x;
+	float acc_y; bool read_acc_y;
+	float acc_z; bool read_acc_z;
+	void read_acc();
+
+	// Magnetometer
+	static const float mag_per_lsb;
+	static const uint8_t reg_mag_x_addr = 0x0E;
+	static const uint8_t reg_mag_y_addr = 0x10;
+	static const uint8_t reg_mag_z_addr = 0x12;
+	float mag_x; bool read_mag_x;
+	float mag_y; bool read_mag_y;
+	float mag_z; bool read_mag_z;
+	void read_mag();
+
+	// Gyroscope
+	static const float gyr_per_lsb;
+	static const uint8_t reg_gyr_x_addr = 0x14;
+	static const uint8_t reg_gyr_y_addr = 0x16;
+	static const uint8_t reg_gyr_z_addr = 0x18;
+	float gyr_x; bool read_gyr_x;
+	float gyr_y; bool read_gyr_y;
+	float gyr_z; bool read_gyr_z;
+	void read_gyr();
+
+	// I2C Euler Angle Registers
+	static const float eul_per_lsb;
+	static const uint8_t reg_eul_h_addr = 0x1A;
+	static const uint8_t reg_eul_r_addr = 0x1C;
+	static const uint8_t reg_eul_p_addr = 0x1E;
+	float eul_h; bool read_eul_h;
+	float eul_r; bool read_eul_r;
+	float eul_p; bool read_eul_p;
+	void read_eul();
+
+	// I2C Quaternion Registers
+	static const float qua_per_lsb;
+	static const uint8_t reg_qua_w_addr = 0x20;
+	static const uint8_t reg_qua_x_addr = 0x22;
+	static const uint8_t reg_qua_y_addr = 0x24;
+	static const uint8_t reg_qua_z_addr = 0x26;
+	float qua_w; bool read_qua_w;
+	float qua_x; bool read_qua_x;
+	float qua_y; bool read_qua_y;
+	float qua_z; bool read_qua_z;
+	void read_qua();
+
+	// I2C Linear Acceleration Registers
+	static const uint8_t reg_lia_x_addr = 0x28;
+	static const uint8_t reg_lia_y_addr = 0x2A;
+	static const uint8_t reg_lia_z_addr = 0x2C;
+	float lia_x; bool read_lia_x;
+	float lia_y; bool read_lia_y;
+	float lia_z; bool read_lia_z;
+	void read_lia();
+
+	// I2C Gravity Vector Registers
+	static const uint8_t reg_grv_x_addr = 0x2E;
+	static const uint8_t reg_grv_y_addr = 0x30;
+	static const uint8_t reg_grv_z_addr = 0x32;
+	float grv_x; bool read_grv_x;
+	float grv_y; bool read_grv_y;
+	float grv_z; bool read_grv_z;
+	void read_grv();
 };
